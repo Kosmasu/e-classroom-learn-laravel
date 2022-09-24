fb487e675d34f66a7ea6418a90751e4bdf7c0056
Kenny <ken_link26@yahoo.com>
2022-09-24 22:43:04 +0700

    Update sidebar.blade.php

--- resources/views/layout/admin/sidebar.blade.php
+++ resources/views/layout/admin/sidebar.blade.php
@@ -6,24 +6,24 @@
     </div>
     <div>
       <a href="{{ route('admin.home') }}">
-        <div class="hover:bg-navy-secondary hover:font-semibold rounded px-1 py-2 pl-2
-        @if (URL::current() == 'http://127.0.0.1:8000/admin') bg-navy-secondary font-semibold @endif
-        ">Home</div>
+        <div class="hover:bg-navy-secondary hover:font-bold font-semibold text-lg rounded px-1 py-2 pl-2
+        {{ (URL::current() == 'http://127.0.0.1:8000/admin') ? 'bg-navy-secondary font-bold' : '' }}
+        "><i class="fa-solid fa-house pr-1"></i> Home</div>
       </a>
     </div>
     <hr>
 
     <div>
-      <h1 class="text-lg font-semibold px-1 py-2">Master</h1>
+      <h1 class="text-lg font-semibold px-1 py-2 pl-2"><i class="fa-solid fa-book-open pr-1"></i> Master</h1>
       <div class="pl-2">
         <a href="{{ route('admin.mahasiswa') }}">
           <div class="hover:bg-navy-secondary hover:font-semibold rounded px-1 py-2 pl-2
-          @if (URL::current() == 'http://127.0.0.1:8000/admin/mahasiswa') bg-navy-secondary font-semibold @endif
+          {{ (URL::current() == 'http://127.0.0.1:8000/admin/mahasiswa') ? 'bg-navy-secondary font-semibold' : '' }}
           ">Mahasiswa</div>
         </a>
         <a href="{{ route('admin.dosen') }}">
           <div class="hover:bg-navy-secondary hover:font-semibold rounded px-1 py-2 pl-2
-          @if (URL::current() == 'http://127.0.0.1:8000/admin/dosen') bg-navy-secondary font-semibold @endif
+          {{ (URL::current() == 'http://127.0.0.1:8000/admin/dosen') ? 'bg-navy-secondary font-semibold' : '' }}
           ">Dosen</div>
         </a>
       </div>
