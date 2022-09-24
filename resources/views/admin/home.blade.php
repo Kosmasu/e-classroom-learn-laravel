098a7642fe94756180c5bac715d30fd3315cd438
Kenny <ken_link26@yahoo.com>
2022-09-24 21:27:44 +0700

    fix error

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.dosen.main')
+
+@section('content')
+  <div class="container">
+    <h1 class="text-xl font-semibold">Ini Home Admin</h1>
+  </div>
+@endsection
--- resources/views/layout/admin/main.blade.php
+++ resources/views/layout/admin/main.blade.php
@@ -1,7 +1,6 @@
 @extends('layout.skeleton')
 
 @section('body')
-  @include('layout.mahasiswa.navbar')
   <main class="pt-4">
     @yield('content')
   </main>
