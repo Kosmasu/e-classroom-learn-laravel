56453996e9591b6c7b4597d402358debdff44ff0
Kenny <ken_link26@yahoo.com>
2022-09-24 20:51:19 +0700

    fix navbar, added banner to login

--- resources/views/layout/user/mahasiswa/navbar.blade.php
+++ resources/views/layout/user/mahasiswa/navbar.blade.php
@@ -7,16 +7,29 @@
       </div>
     </a>
     <a href="{{ route('mahasiswa.profile') }}" class="ml-auto">
-      <div class="flex items-center h-full">
+      <div class="dropdown inline-block relative flex items-center h-full">
         <div
-          class="relative pl-4 pr-1 py-1 rounded-full bg-gray-200  flex justify-center items-center hover:cursor-pointer hover:bg-gray-300">
+          class="pl-4 pr-1 py-1 rounded-full bg-gray-200  flex justify-center items-center hover:cursor-pointer hover:bg-gray-300">
           <span class="font-semibold text-navy-primary uppercase">Kenny</span>
           <div
             class="w-10 h-10 ml-2 p-2 rounded-full bg-navy-primary flex justify-center items-center text-navy-primary">
             <i class="text-gray-200 fa-solid fa-user text-xl"></i>
           </div>
         </div>
+        <div class="dropdown-content absolute w-32 right-0 bottom-0 translate-y-full shadow rounded bg-navy-primary border hidden">
+          <div class="flex flex-col font-semibold">
+            <a class="px-3 py-2 hover:bg-navy-secondary" href="{{ route('mahasiswa.home') }}">Home</a>
+            <a class="px-3 py-2 hover:bg-navy-secondary" href="{{ route('mahasiswa.profile') }}">Profile</a>
+            <a class="px-3 py-2 hover:bg-navy-secondary" href="{{ route('auth.login') }}">Logout</a>
+          </div>
+        </div>
       </div>
     </a>
   </div>
 </nav>
+
+<style>
+.dropdown:hover .dropdown-content {
+  display: block
+}
+</style>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,25 +1,38 @@
 @extends('layout.skeleton')
 
 @section('body')
-  <div class="h-screen flex justify-center items-center bg-slate-400">
-    <div class="p-8 rounded lg:rounded-lg shadow lg:shadow-lg w-80 bg-navy-primary text-gray-200">
-      <div>
-        <h1 class="text-3xl font-bold text-center">Login</h1>
-      </div>
-      <div class="flex flex-col mt-4">
-        <label class="w-full px-1 mt-2" for="username">Username</label>
-        <input class="w-full mt-1 rounded px-1 py-1 bg-gray-200" type="text" name="username" id="username" placeholder="Username">
-        <label class="w-full px-1 mt-2" for="password">Password</label>
-        <input class="w-full mt-1 rounded px-2 py-1 bg-gray-200"  type="password" name="password" id="password" placeholder="Password">
-        {{-- <div class="flex justify-end"> --}}
-        <button class="w-full px-2 py-1 mt-4 bg-gray-200 rounded text-gray-900 font-medium hover:bg-gray-300 active:bg-gray-400 ">Submit</button>
-        {{-- </div> --}}
-      </div>
-      <div class="mt-1">
-        <p class="text-xs">
-          Don't have an account? register
-          <a class="text-yellow-primary hover:underline" href="{{ route('register.mahasiswa') }}">here</a>
-        </p>
+  <div class="w-screen h-screen bg-no-repeat bg-cover relative"
+    style="background-image: url('{{ asset('images/banner.jpg') }}');">
+    <div class="h-screen flex justify-center items-center">
+      <span class="w-full h-full bg-black opacity-50 absolute z-0"></span>
+      <div class="p-8 rounded lg:rounded-lg shadow lg:shadow-lg w-80 bg-navy-primary text-gray-200 z-10">
+        <div>
+          <h1 class="text-3xl font-bold text-center">Login</h1>
+        </div>
+        <div class="flex flex-col mt-4">
+          <label class="w-full px-1 mt-2" for="username">Username</label>
+          <input class="w-full mt-1 rounded px-1 py-1 bg-gray-200" type="text" name="username" id="username"
+            placeholder="Username">
+          <label class="w-full px-1 mt-2" for="password">Password</label>
+          <input class="w-full mt-1 rounded px-2 py-1 bg-gray-200" type="password" name="password" id="password"
+            placeholder="Password">
+            <a href="{{ route('dosen.home') }}">
+              <button class="w-full px-2 py-1 mt-4 bg-gray-200 rounded text-gray-900 font-medium hover:bg-gray-300 active:bg-gray-400">Dosen</button>
+            </a>
+            <a href="{{ route('mahasiswa.home') }}">
+              <button class="w-full px-2 py-1 mt-4 bg-gray-200 rounded text-gray-900 font-medium hover:bg-gray-300 active:bg-gray-400">Mahasiswa</button>
+            </a>
+            <a href="{{ route('') }}">
+              <button class="w-full px-2 py-1 mt-4 bg-gray-200 rounded text-gray-900 font-medium hover:bg-gray-300 active:bg-gray-400">Admin</button>
+            </a>
+          <span class="text-xs">Button sementara untuk keperluan navigasi</span>
+        </div>
+        <div class="mt-1">
+          <p class="text-xs">
+            Don't have an account? register
+            <a class="text-yellow-primary hover:underline" href="{{ route('auth.register.mahasiswa') }}">here</a>
+          </p>
+        </div>
       </div>
     </div>
   </div>
